0623c956eaec3915fa6619e71c49f924f264322d
sweetfood-dev <kjsii123@hanmail.net>
2021-10-04 18:03:35 +0900

    Initial Commit

---
+++ SelfSizing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfSizing
+//
+//  Created by 권지수 on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
